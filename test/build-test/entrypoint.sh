b74d79a03902bf84bade24c6f4f15090e250645c
Unknown <guillem.lion.benosa@gmail.com>
2018-08-17 21:28:31 +0200

    added tests

--- test/build-test/entrypoint.sh
+++ test/build-test/entrypoint.sh
@@ -1,20 +1,39 @@
 #!/bin/sh
 
-check_domain()
-{
-    DOMAIN=$1
-    EXPECTED_IP=$2
-    IP=$(dig "$DOMAIN" +short)
-    if [ "$IP" = "$EXPECTED_IP" ]; then
-        echo "resolved $DOMAIN to: $IP"
+# Before test
+FILE_PATH=hello.txt
+echo "DAppNodeIsAwesome" > $FILE_PATH
+
+# Test add
+HASH=$(curl -F file=@${FILE_PATH} "http://${IPFS_IP}:5001/api/v0/add" | jq -r '.Hash')
+if [ -z "$HASH" ]
+then
+    echo "HASH receibed after adding file is empty"
+    exit 1
+else
+    echo "Added $FILE_PATH, HASH: $HASH"
+fi
+
+# Test cat
+FILE=$(curl "http://${IPFS_IP}:5001/api/v0/object/data?arg=${HASH}")
+if [ -z "$FILE" ]
+then
+    echo "FILE receibed after catting HASH $HASH is empty"
+    exit 1
+else
+    # Should compare this two to verify it's the correct output
+    # But the string I get is completely uncomparable, and nothing works
+    echo "Received file from HASH: $HASH"
+    echo "${FILE}" | awk '/DAppNodeIsAwesome/{exit 0} !/DAppNodeIsAwesome/{exit 1}' ;
+    if [ "$?" = "0" ]
+    then
+        echo "File contains expected string"
     else
-        echo "Error $DOMAIN resolves to: $IP"
+        echo "File DOES NOT contain expected string"
+        echo "Expected:  DAppNodeIsAwesome"
+        echo "Received:  ${FILE}"
         exit 1
     fi
-}
+fi
 
-check_domain my.ethchain.dnp.dappnode.eth 172.33.1.6
-check_domain ethchain.dappnode.eth 172.33.1.3
-check_domain ethchain.eth 172.33.1.3
-# This one currently fails, should not
-check_domain ethchain.dnp.dappnode.eth 172.33.1.3
+
--- test/test.sh
+++ test/test.sh
@@ -3,4 +3,5 @@
 # Therefore the name of the network has to be dnpbind_network
 
 docker-compose -f docker-compose-ipfs.yml up -d
+docker-compose -f test/docker-compose-test.yml build
 docker-compose -f test/docker-compose-test.yml run test
