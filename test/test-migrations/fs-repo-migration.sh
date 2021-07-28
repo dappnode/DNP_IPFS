08695d4f3d5ef9a26e30649310c24d95e6313b2a
pablo <mendez4a@gmail.com>
2021-07-28 11:17:31 +0200

    added container name and exit on error

--- test/test-migrations/fs-repo-migration.sh
+++ test/test-migrations/fs-repo-migration.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 # This script will test the IPFS file system migration
 # It will migrate volumes from an older IPFS version (e.g v0.6.0)
@@ -16,21 +17,22 @@ IPFS_OLD=v0.6.0
 IPFS_NEW=v0.9.1
 
 # 0. Set-up
-echo echo -e "\e[32m [INFO] Creating dncore_network if needed...\e[0m"
+echo -e "\e[32m [INFO] Creating dncore_network if needed...\e[0m"
 docker network create --driver bridge --subnet 172.33.0.0/16 dncore_network
 
 # 1. Start container with older image to create old volumes
-echo echo -e "\e[32m [INFO] Build old ipfs ${IPFS_OLD}\e[0m"
+echo -e "\e[32m [INFO] Build old ipfs ${IPFS_OLD}\e[0m"
 docker-compose build --build-arg UPSTREAM_VERSION=${IPFS_OLD}
 docker-compose -f docker-compose.yml up -d
+sleep 10
 docker-compose down
 
 # 2. Start container with newer image to migrate old volumes
-echo echo -e "\e[32m [INFO] Build new ipfs ${IPFS_NEW}\e[0m"
+echo -e "\e[32m [INFO] Build new ipfs ${IPFS_NEW}\e[0m"
 docker-compose build --build-arg UPSTREAM_VERSION=${IPFS_NEW}
 docker-compose -f docker-compose.yml up -d
 sleep 10
 
-echo echo -e "\e[32m [INFO] Check logs for migration\e[0m"
+echo -e "\e[32m [INFO] Check logs for migration\e[0m"
 docker logs DAppNodeCore-ipfs-dnp.dappnode.eth
 docker-compose down
