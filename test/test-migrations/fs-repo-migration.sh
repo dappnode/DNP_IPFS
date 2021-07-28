25862f2772f58b634f026810ce004e64c005f7d5
pablo <mendez4a@gmail.com>
2021-07-28 11:12:48 +0200

    added logs to test

--- test/test-migrations/fs-repo-migration.sh
+++ test/test-migrations/fs-repo-migration.sh
@@ -11,12 +11,26 @@
 # |  0.6.0 - 0.7.0  |    10      |  v0.2.14 |
 # | 0.8.0 - current |    11      |  v0.2.15 |
 
+# Envs:
+IPFS_OLD=v0.6.0
+IPFS_NEW=v0.9.1
+
+# 0. Set-up
+echo echo -e "\e[32m [INFO] Creating dncore_network if needed...\e[0m"
+docker network create --driver bridge --subnet 172.33.0.0/16 dncore_network
+
 # 1. Start container with older image to create old volumes
-docker-compose build --build-arg UPSTREAM_VERSION=v0.6.0
+echo echo -e "\e[32m [INFO] Build old ipfs ${IPFS_OLD}\e[0m"
+docker-compose build --build-arg UPSTREAM_VERSION=${IPFS_OLD}
 docker-compose -f docker-compose.yml up -d
 docker-compose down
 
 # 2. Start container with newer image to migrate old volumes
-docker-compose build --build-arg UPSTREAM_VERSION=v0.9.1
+echo echo -e "\e[32m [INFO] Build new ipfs ${IPFS_NEW}\e[0m"
+docker-compose build --build-arg UPSTREAM_VERSION=${IPFS_NEW}
 docker-compose -f docker-compose.yml up -d
+sleep 10
+
+echo echo -e "\e[32m [INFO] Check logs for migration\e[0m"
+docker logs DAppNodeCore-ipfs-dnp.dappnode.eth
 docker-compose down
