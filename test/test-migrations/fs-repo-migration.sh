a5150aa37b7937e6e8e57ad5e821ef317b624bfa
pablo <mendez4a@gmail.com>
2021-07-28 11:31:49 +0200

    sleep bigger

--- test/test-migrations/fs-repo-migration.sh
+++ test/test-migrations/fs-repo-migration.sh
@@ -17,22 +17,22 @@ IPFS_OLD=v0.6.0
 IPFS_NEW=v0.9.1
 
 # 0. Set-up
-echo -e "\e[32m [INFO] Creating dncore_network if needed...\e[0m"
+echo -e "\e[34m [INFO] Creating dncore_network if needed...\e[0m"
 docker network create --driver bridge --subnet 172.33.0.0/16 dncore_network
 
 # 1. Start container with older image to create old volumes
-echo -e "\e[32m [INFO] Build old ipfs ${IPFS_OLD}\e[0m"
+echo -e "\e[34m [INFO] Build old ipfs ${IPFS_OLD}\e[0m"
 docker-compose build --build-arg UPSTREAM_VERSION=${IPFS_OLD}
 docker-compose -f docker-compose.yml up -d
 sleep 10
 docker-compose down
 
 # 2. Start container with newer image to migrate old volumes
-echo -e "\e[32m [INFO] Build new ipfs ${IPFS_NEW}\e[0m"
+echo -e "\e[34m [INFO] Build new ipfs ${IPFS_NEW}\e[0m"
 docker-compose build --build-arg UPSTREAM_VERSION=${IPFS_NEW}
 docker-compose -f docker-compose.yml up -d
-sleep 10
+sleep 30
 
-echo -e "\e[32m [INFO] Check logs for migration\e[0m"
+echo -e "\e[34m [INFO] Check logs for migration\e[0m"
 docker logs DAppNodeCore-ipfs.dnp.dappnode.eth
 docker-compose down
