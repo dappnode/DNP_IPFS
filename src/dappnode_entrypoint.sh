a93b26f21cd999bba0c563b9025b979eea68c6a8
Pablo Mendez <mendez4a@gmail.com>
2023-07-04 09:58:48 +0200

    add fs repo mnigrations

--- src/dappnode_entrypoint.sh
+++ src/dappnode_entrypoint.sh
@@ -7,6 +7,8 @@ user=ipfs
 # IPFS_PATH=/data/ipfs
 repo="$IPFS_PATH"
 
+fs-repo-migrations
+
 # If the user changes then the volumes could not be used and the ipfs init will fail
 if [ $(id -u) -eq 0 ]; then
     echo "Changing user to $user"
@@ -54,14 +56,6 @@ ipfs config --json Datastore.StorageMax "\"$DATASTORE_STORAGEMAX\""
 
 ## Provides origin isolation between content roots: https://github.com/ipfs/kubo/blob/master/docs/config.md#gatewaypublicgateways-usesubdomains
 ipfs config --json Gateway.PublicGateways '{"ipfs.dappnode": { "NoDNSLink": false, "Paths": [ "/ipfs" , "/ipns" ], "UseSubdomains": true }}'
-
-# Add handler
-sigterm_handler() {
-    echo -e "Caught singal. Stopping ipfs service gracefully"
-    exit 0
-}
-
-trap 'sigterm_handler' TERM INT
 
 # Possible values for EXTRA_OPTS (must have --): https://docs.ipfs.io/reference/cli/#ipfs-daemon
 # Join arguments with EXTRA_OPTS if var not empty.
