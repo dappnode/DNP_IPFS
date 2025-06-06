8d90ff980f4a4b813a43deb184ba83260f40744b
pablomendezroyo <41727368+pablomendezroyo@users.noreply.github.com>
2025-06-06 09:55:25 +0200

    Add dappnode config (#143)
    * Add control config for methods
    
    * use wildcard
    
    * download fs-repo-migrations tool
    
    * add logging
    
    * add peer ids
    
    * list bootstrap list
    
    ---------
    
    Co-authored-by: Pablo Mendez <pablo@dappnode.io>

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -4,6 +4,8 @@ set -e
 user=ipfs
 repo="$IPFS_PATH"
 
+echo "Starting IPFS entrypoint script"
+
 if [ "$(id -u)" -eq 0 ]; then
   echo "Changing user to $user"
   # ensure folder is writable
@@ -12,9 +14,10 @@ if [ "$(id -u)" -eq 0 ]; then
   exec gosu "$user" "$0" "$@"
 fi
 
+echo "Running as user: $(id -un)"
+
 # 2nd invocation with regular user
 ipfs version
-
 
 if [ -e "$repo/config" ]; then
   echo "Found IPFS fs-repo at $repo"
@@ -51,7 +54,19 @@ else
 fi
 
 # IMPORTANT: this is a copy of the original entrypoint to add dappnode custom config to inject our gateways
+# In order to be able to execute config commands, the fs-repo-migrations must be run first
+echo "Running fs-repo-migrations"
+/usr/local/bin/fs-repo-migrations -y
+echo "Running IPFS config commands"
+ipfs config --json API.HTTPHeaders.Access-Control-Allow-Origin '["*"]'
+ipfs config --json API.HTTPHeaders.Access-Control-Allow-Methods '["PUT", "GET", "POST"]'
 ipfs config --json Gateway.PublicGateways '{"ipfs.dappnode": { "NoDNSLink": false, "Paths": [ "/ipfs" , "/ipns" ], "UseSubdomains": false }}'
+# Add to bootstrap list the production Dappnode IPFS gateway
+ipfs bootstrap add /ip4/65.109.51.31/tcp/4001/p2p/12D3KooWLdrSru7LzYY4YDcfnJsrJeshTQooR2j38NkGvoj2yADp
+# Add to bootstrap list the dev Dappnode IPFS gateway 
+ipfs bootstrap add /ip4/167.86.114.131/tcp/4001/p2p/12D3KooWCAx5zWejUDotqc7dcvpvNstM9eZRdtdne1oXZ1DpdLFb
+# list bootstrap peers
+ipfs bootstrap list
 
 find /container-init.d -maxdepth 1 -type f -iname '*.sh' -print0 | sort -z | xargs -n 1 -0 -r container_init_run
 
